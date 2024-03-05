dc7e8c23667b7205d69e1e5f14ec7c4407ed1dda
hhejo <litsmainjof@gmail.com>
2024-03-05 20:28:18 +0900

    feat: 진입점 추가

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,32 +1,31 @@
-import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { toast } from "react-toastify";
-
-import userApi from "../../api/userApi";
-import { getUser } from "../../redux/userSlice";
-
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+import { setCurrentUser } from "../../redux/user-slice";
 import Header from "../../components/Header";
 import LoginTitle from "./LoginTitle";
 import LoginForm from "./LoginForm";
 
+// Login
 const Login = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  const [dispatch, navigate] = [useDispatch(), useNavigate()];
 
+  // loginHandler
   const loginHandler = async (loginData) => {
     try {
-      const res = await userApi.login(loginData);
-      localStorage.setItem("access-token", `${res.data.jwt}`);
-      dispatch(getUser());
-      toast.success("로그인 성공");
-      navigate("/teams");
-    } catch (err) {
-      if (err.response.status === 409) {
-        toast.warning("해당 아이디가 존재하지 않거나 비밀번호가 틀립니다");
-      } else {
-        toast.error("Error");
-      }
+      const { email, password: pw } = loginData; // 입력한 email, password 가져오기
+      const { user } = await signInWithEmailAndPassword(auth, email, pw); // firebase에 email, password가 일치하는 유저 가져옴
+      dispatch(setCurrentUser(user)); // Redux에 user 정보 저장
+      toast.success("로그인 성공"); // 토스트 출력
+      navigate("/teams"); // /temas로 이동
+    } catch (error) {
+      const { code: errorCode, message: errorMessage } = error; // 로그인 에러
+      if (errorCode === "auth/invalid-credential")
+        toast.warning("유효하지 않은 이메일이나 비밀번호입니다."); // 409
+      else toast.error("로그인 오류");
+      console.error(errorMessage);
     }
   };
 
--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,32 +1,34 @@
-import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { toast } from "react-toastify";
-
-import userApi from "../../api/userApi";
-import { getUser } from "../../redux/userSlice";
-
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth } from "../../firebase";
+import { setCurrentUser } from "../../redux/user-slice";
 import Header from "../../components/Header";
 import SignupTitle from "./SignupTitle";
 import SignupForm from "./SignupForm";
 
+// Signup
 const Signup = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  const [dispatch, navigate] = [useDispatch(), useNavigate()];
 
+  // signupHandler
   const signupHandler = async (signupData) => {
     try {
-      const res = await userApi.signup(signupData);
-      localStorage.setItem("access-token", `${res.data.jwt}`);
-      dispatch(getUser());
-      toast.success("회원가입 성공");
-      navigate("/teams");
-    } catch (err) {
-      if (err.response.status === 409) {
-        toast.warning("이미 해당 아이디가 존재합니다");
-      } else {
-        toast.error("Error");
-      }
+      const { email, password: pw, nickname } = signupData; // 입력한 email, password, nickname 가져오기
+      const { user } = await createUserWithEmailAndPassword(auth, email, pw); // firebase에 email, password를 가진 유저 생성
+      await updateProfile(user, { displayName: nickname }); // 닉네임 적용
+      dispatch(setCurrentUser(user)); // Redux에 user 정보 저장
+      toast.success("회원가입 성공"); // 토스트 출력
+      navigate("/teams"); // /teams로 이동
+    } catch (error) {
+      const { code: errorCode, message: errorMessage } = error; // 회원가입 에러
+      if (errorCode === "auth/weak-password")
+        toast.warning("비밀번호는 6자 이상이어야 합니다.");
+      else if (errorCode === "auth/email-already-in-use")
+        toast.warning("이미 사용중인 이메일입니다."); // 409
+      else toast.error("회원가입 오류");
+      console.error(errorMessage);
     }
   };
 
