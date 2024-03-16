52a8e1a728431495559e97ad1ba801e404f0b143
hhejo <litsmainjof@gmail.com>
2024-03-16 21:46:03 +0900

    feat: Private Route 설정

--- src/route/PrivateRoute.jsx
+++ src/route/PrivateRoute.jsx
@@ -1,8 +1,12 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 const PrivateRoute = () => {
   const { isLoggedIn } = useSelector((state) => state.user.value);
+  useEffect(() => {
+    console.log("Private Route");
+  }, []);
   if (isLoggedIn) return <Outlet />;
   return <Navigate to="/" replace />;
 };
