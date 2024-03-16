fb1aeb3edc14f10c1fff252eb2676ac6c35124d8
hhejo <litsmainjof@gmail.com>
2024-03-16 21:46:15 +0900

    feat: Protected Route 설정

--- src/route/ProtectedRoute.jsx
+++ src/route/ProtectedRoute.jsx
@@ -1,8 +1,12 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 const ProtectedRoute = () => {
   const { isLoggedIn } = useSelector((state) => state.user.value);
+  useEffect(() => {
+    console.log("Protected Route");
+  }, []);
   if (isLoggedIn) return <Navigate to="/teams" replace />;
   return <Outlet />;
 };
