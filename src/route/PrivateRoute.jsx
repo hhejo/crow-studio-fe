58e2d62ee5d7d3c63c652bff7291a2c5c4c59577
hhejo <litsmainjof@gmail.com>
2024-03-17 10:07:08 +0900

    refactor: Private, Protected Route 정리

--- src/route/PrivateRoute.jsx
+++ src/route/PrivateRoute.jsx
@@ -1,14 +1,10 @@
-import { useEffect } from "react";
 import { Navigate, Outlet, useOutletContext } from "react-router-dom";
 
 const PrivateRoute = () => {
-  const { loggedIn } = useOutletContext();
-
-  useEffect(() => {
-    console.log(`Private Route, isLoggedIn:${loggedIn}`);
-  }, [loggedIn]);
-
+  const { loggedIn } = useOutletContext(); // Outlet으로 전달된 loggedIn
+  // 로그인 되었으면 로그인 필요한 모든 서비스에 접근
   if (loggedIn) return <Outlet />;
+  // 로그인 되지 않았으면 /intro로 리다이렉트
   return <Navigate to="/intro" replace />;
 };
 
--- src/route/ProtectedRoute.jsx
+++ src/route/ProtectedRoute.jsx
@@ -1,14 +1,10 @@
-import { useEffect } from "react";
 import { Navigate, Outlet, useOutletContext } from "react-router-dom";
 
 const ProtectedRoute = () => {
-  const { loggedIn } = useOutletContext();
-
-  useEffect(() => {
-    console.log(`Protected Route, loggedIn:${loggedIn}`);
-  }, [loggedIn]);
-
+  const { loggedIn } = useOutletContext(); // Outlet으로 전달된 loggedIn
+  // 로그인 되었으면 /teams로 이동
   if (loggedIn) return <Navigate to="/teams" replace />;
+  // 로그인 되지 않았으면, 로그인 없이 가능한 서비스에 접근
   return <Outlet />;
 };
 
